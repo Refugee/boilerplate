fa9b19e980ed30fba976b24403c0d1bc4a0529e3
Rafael Birkmann <mail@rafaelbirkmann.de>
2014-10-20 16:41:56 +0200

    added basic sass file structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,46 @@
 module.exports = function(grunt) {
 
 	grunt.initConfig({
+
 		pkg: grunt.file.readJSON('package.json'),
+
+  	sass: {
+			dist: {
+				files: {
+					'build/css/main.css': 'source/sass/*.scss'
+				}
+			}
+    },
+
 		uglify: {
     	my_target: {
       	files: {
 					'build/js/main.min.js': ['source/js/jquery.js', 'source/js/main.js']
 				}
 			}
-  	}
+  	},
+
+  	watch: {
+			scripts: {
+				files: [
+					'source/sass/**/*.scss'
+				],
+				tasks: ['sass']
+			}
+    },
+
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.registerTask('default', ['uglify']);
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['build']);
+
+	grunt.registerTask('build', [
+		'sass',
+		'uglify',
+		'watch'
+	]);
 
 };
